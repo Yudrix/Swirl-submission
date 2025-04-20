6c36087663648bd5fde2bc9e22f6a7e5fab1a0f5
xxzytu <Riddhiman.reddy@gmail.com>
2025-04-20 17:43:18 +0530

    3 pages + js + 404 + animations

---
+++ footer.js
@@ -0,0 +1,6 @@
+document.write(
+    `<footer class="footer">
+    <p>Made with ❤️ by <a href="https://github.com/Yudrix">Yudi</a> for <a href="https://swirl.hackclub.com/">Swirl!</a></p>
+    <p>&copy; 2025 Yudrix All rights reserved.</p>
+    </footer>`
+);
